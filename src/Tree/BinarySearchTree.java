0cdef9d4a54acb6f17e125b4b433a313a5cf3a6e
Vahe <vahe.aghajanyan@arca.am>
2023-10-25 14:08:02 +0400

    Tree implementation.

---
+++ src/Tree/BinarySearchTree.java
@@ -0,0 +1,112 @@
+/**
+ * Created by Vahe Aghajanyan
+ * Date: 10/25/2023
+ * Time: 10:51 AM
+ */
+
+package Tree;
+
+class Node {
+    int key;
+    Node left;
+    Node right;
+
+    public Node(int key) {
+        this.key = key;
+        left = null;
+        right = null;
+    }
+}
+
+public class BinarySearchTree {
+    private Node root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    private Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key < root.key) {
+            root.left = insertRec(root.left, key);
+        } else if (key > root.key) {
+            root.right = insertRec(root.right, key);
+        }
+
+        return root;
+    }
+
+    public boolean search(int key) {
+        return searchRec(root, key);
+    }
+
+    private boolean searchRec(Node root, int key) {
+        if (root == null || root.key == key) {
+            return root != null;
+        }
+
+        if (key < root.key) {
+            return searchRec(root.left, key);
+        }
+
+        return searchRec(root.right, key);
+    }
+
+    public void inorderTraversal() {
+        inorderTraversalRec(root);
+    }
+
+    private void inorderTraversalRec(Node root) {
+        if (root != null) {
+            inorderTraversalRec(root.left);
+            System.out.print(root.key + " ");
+            inorderTraversalRec(root.right);
+        }
+    }
+
+    public void delete(int key) {
+        root = deleteRec(root, key);
+    }
+
+    private Node deleteRec(Node root, int key) {
+        if (root == null) {
+            return root;
+        }
+
+        if (key < root.key) {
+            root.left = deleteRec(root.left, key);
+        } else if (key > root.key) {
+            root.right = deleteRec(root.right, key);
+        } else {
+            // Node with only one child or no child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // Node with two children
+            root.key = minValue(root.right);
+            root.right = deleteRec(root.right, root.key);
+        }
+
+        return root;
+    }
+
+    private int minValue(Node root) {
+        int minValue = root.key;
+        while (root.left != null) {
+            minValue = root.left.key;
+            root = root.left;
+        }
+        return minValue;
+    }
+}
